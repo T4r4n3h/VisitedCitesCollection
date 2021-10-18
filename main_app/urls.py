9efbd5dafaa9dcb2a9f2c9c74eb9525460c37ebb
Taraneh Saleh <taranehsaleh@Taranehs-MacBook-Pro.local>
2021-10-18 17:35:30 -0400

    lab work#2 complete

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('about/', views.about, name='about'),
+    path('about/', views.about, name='about'),
     path('cities/', views.cities_index, name='cities'),
+    path('cities/<int:city_id>', views.city_detail, name='detail')
+    
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,34 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from .models import City
 
 #Define the home views 
 # def home(request):
 #     return HttpResponse('<h1> Hello to Cities Ive been to</h1>')
 
 def home(request):
+    return render(request, 'home.html')
+
+def about(request):
     return render(request, 'about.html')
-
-class City:
-    def __init__(self, name, country, rank):
-        self.name = name
-        self.country = country
-        self.rank = rank
-        
-
-
-cities = [
-    City('Paris', 'France', 4),
-    City('Valencia', 'Spain', 5),
-    City('New York', 'United States', 4)
-
-]
-
-
-
-
-
-
+    
 def cities_index(request):
-    return render(request, 'cities/index.html', {'cities':cities})
+    cities = City.objects.all()
+    print('this is the list of cities:', cities)
+    return render(request, 'cities/index.html',{
+        'cities': cities,
+    })
+
+def city_detail(request, city_id):
+    city = City.objects.get(id=city_id)
+    print('this is the city: ', city)
+    return render(request, 'cities/detail.html',{
+        'city': city,
+    })
+
